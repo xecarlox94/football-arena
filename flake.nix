4fe28f175cc3ed1fc2a1c1a5dfcae2aaeabd2174
xecarlox94 <jf94.uk@gmail.com>
2025-11-01 20:26:23 +0000

    fixed assets folder

--- flake.nix
+++ flake.nix
@@ -17,12 +17,34 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
+
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        download-assets-script = pkgs.writeShellApplication {
+          name="download-assets-script";
+          runtimeInputs= with pkgs; [wget unzip];
+          text=''
+            rm -rf ./assets/
+            mkdir ./assets/
+            wget -O ./assets/master.zip https://github.com/UnravelSports/rs-football-3d/archive/cab54d6d2cd6416eb881cf542f93d1af838bcddd.zip
+
+            cd ./assets/
+            unzip master.zip
+            rm master.zip
+            cp -r rs-football-3d-cab54d6d2cd6416eb881cf542f93d1af838bcddd/assets/* .
+
+            rm -r rs-football-3d-cab54d6d2cd6416eb881cf542f93d1af838bcddd/
+
+          '';
+        };
+
+
       in
       {
+
         devShells.default =
           with pkgs;
           mkShell {
@@ -31,6 +53,7 @@
                 # Rust dependencies
                 (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
                 pkg-config
+                download-assets-script
               ]
               ++ lib.optionals (lib.strings.hasInfix "linux" system) [
                 # for Linux
